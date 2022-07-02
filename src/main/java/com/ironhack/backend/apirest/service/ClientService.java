d79ea6b65966753e60139a58ff4c1eedaa902987
Angie <angiejimenez@gmail.com>
2022-07-02 20:08:58 +0200

    .

--- src/main/java/com/ironhack/backend/apirest/service/ClientService.java
+++ src/main/java/com/ironhack/backend/apirest/service/ClientService.java
@@ -4,16 +4,14 @@ import com.ironhack.backend.apirest.models.Client;
 import com.ironhack.backend.apirest.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
 public class ClientService implements IClientService {
 
-@Autowired
-private ClientRepository clientRepository;
+    @Autowired
+    private ClientRepository clientRepository;
 
     @Override
     public List<Client> findAll() {
