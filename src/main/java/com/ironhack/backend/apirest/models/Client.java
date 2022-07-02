18657a618ac1fd9717dcf5622ae1f93935aaa0e7
Angie <angiejimenez@gmail.com>
2022-07-02 14:32:12 +0200

    project configuration

---
+++ src/main/java/com/ironhack/backend/apirest/models/Client.java
@@ -0,0 +1,61 @@
+package com.ironhack.backend.apirest.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String surname;
+    private String email;
+    @Column(name= "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
---
+++ src/test/java/com/ironhack/backend/apirest/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
